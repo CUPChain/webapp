5c301010d824e4196a76dc8221bbc7c6a7a7fa9d
antoandgar <antonioandgargiulo@gmail.com>
2023-12-20 16:57:13 +0100

    basics of Back-End

--- backend/main.py
+++ backend/main.py
@@ -3,9 +3,34 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return {'hello': 'world'}
+@app.route("/")
+def login():
+    # here we have the login page
+
+    return {"hello": "world"}
+
+
+@app.route("/appointment/")  # add <user_id> to the path
+def book_appointments():
+    # here we have the booked appointment page
+    # and the list of available prescriptions
+
+    return {"appointment": "bla bla"}
+
+
+@app.route("/reservation_info/")  # add <reservation_id> to the path
+def reservation_info():
+    # here we have the information about the reservation
+    # ith the qr and the stuff about the verification on the blockchain
+
+    return {"reservation_info": "bla bla"}
+
+
+@app.route("/account/")  # add <user_id> to the path
+def account():
+    # here we have the account page
+
+    return {"account": "bla bla"}
 
 
 if __name__ == "__main__":
