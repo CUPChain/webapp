9dc8bc88a9d3443bcba32f922da1795b682d2516
antoandgar <antonioandgargiulo@gmail.com>
2024-01-03 20:04:04 +0100

    revise some code changes in api and use best practices of sqlalchemy, still untested

--- backend/db.py
+++ backend/db.py
@@ -12,7 +12,7 @@ db = SQLAlchemy(app)
 # Define your models
 class Doctor(db.Model):
     __tablename__ = "doctor"
-    cf = db.Column(db.String(10), primary_key=True)
+    cf = db.Column(db.String(16), primary_key=True)
     name = db.Column(db.String(50))
     surname = db.Column(db.String(50))
     residence = db.Column(db.String(100))
@@ -21,15 +21,15 @@ class Doctor(db.Model):
 class Prescription(db.Model):
     __tablename__ = "prescription"
     id = db.Column(db.Integer, primary_key=True)
-    cf_Patient = db.Column(db.String(10), db.ForeignKey("patient.cf"))
-    code_Medical_examination = db.Column(
+    cf_Patient = db.Column(db.String(16), db.ForeignKey("patient.cf"))
+    code_medical_examination = db.Column(
         db.String(10), db.ForeignKey("medical_exam.code")
     )
 
 
 class Patient(db.Model):
     __tablename__ = "patient"
-    cf = db.Column(db.String(10), primary_key=True)
+    cf = db.Column(db.String(16), primary_key=True)
     name = db.Column(db.String(50))
     surname = db.Column(db.String(50))
     residence = db.Column(db.String(100))
@@ -65,9 +65,9 @@ class IsAbleToDo(db.Model):
 
 
 # REST API routes for CRUD operations
-@app.route("/doctors", methods=["GET"])
+@app.route("/api/v1/doctors", methods=["GET"])
 def get_doctors():
-    doctors = Doctor.query.all()
+    result = db.session.execute(db.select(Doctor))
     doctors_list = [
         {
             "cf": doctor.cf,
@@ -75,14 +75,15 @@ def get_doctors():
             "surname": doctor.surname,
             "residence": doctor.residence,
         }
-        for doctor in doctors
+        for doctor in result
     ]
     return jsonify({"doctors": doctors_list})
 
 
-@app.route("/doctors/<cf>", methods=["GET"])
+@app.route("/api/v1/doctors/<cf>", methods=["GET"])
 def get_doctor(cf):
-    doctor = Doctor.query.get(cf)
+    result = db.session.execute(db.select(Doctor).where(Doctor.cf == cf))
+    doctor = result.fetchone()
     if doctor:
         doctor_data = {
             "cf": doctor.cf,
@@ -95,37 +96,38 @@ def get_doctor(cf):
         return jsonify({"message": "Doctor not found"}), 404
 
 
-@app.route("/prescriptions", methods=["GET"])
-def get_prescription():
-    prescriptions = Prescription.query.all()
+@app.route("/api/v1/prescriptions", methods=["GET"])
+def get_prescriptions():
+    result = db.session.execute(db.select(Prescription))
     prescriptions_list = [
         {
             "id": prescription.id,
             "cf_patient": prescription.cf_Patient,
-            "code_medical_examination": prescription.code_Medical_examination,
+            "code_medical_examination": prescription.code_medical_examination,
         }
-        for prescription in prescriptions
+        for prescription in result
     ]
     return jsonify({"prescriptions": prescriptions_list})
 
 
-@app.route("/prescriptions/<id>", methods=["GET"])
+@app.route("/api/v1/prescriptions/<id>", methods=["GET"])
 def get_prescription(id):
-    prescription = Prescription.query.get(id)
+    result = db.session.execute(db.select(Prescription).where(Prescription.id == id))
+    prescription = result.fetchone()
     if prescription:
         prescription_data = {
             "id": prescription.id,
             "cf_patient": prescription.cf_Patient,
-            "code_medical_examination": prescription.code_Medical_examination,
+            "code_medical_examination": prescription.code_medical_examination,
         }
         return jsonify({"prescription": prescription_data})
     else:
         return jsonify({"message": "Prescription not found"}), 404
 
 
-@app.route("/patients", methods=["GET"])
+@app.route("/api/v1/patients", methods=["GET"])
 def get_patients():
-    patients = Patient.query.all()
+    result = db.session.execute(db.select(Patient))
     patients_list = [
         {
             "cf": patient.cf,
@@ -133,14 +135,15 @@ def get_patients():
             "surname": patient.surname,
             "residence": patient.residence,
         }
-        for patient in patients
+        for patient in result
     ]
     return jsonify({"patients": patients_list})
 
 
-@app.route("/patients/<cf>", methods=["GET"])
+@app.route("/api/v1/patients/<cf>", methods=["GET"])
 def get_patient(cf):
-    patient = Patient.query.get(cf)
+    result = db.session.execute(db.select(Patient).where(Patient.cf == cf))
+    patient = result.fetchone()
     if patient:
         patient_data = {
             "cf": patient.cf,
@@ -150,68 +153,73 @@ def get_patient(cf):
         }
         return jsonify({"patient": patient_data})
     else:
-        return jsonify({"message": "patient not found"}), 404
+        return jsonify({"message": "Patient not found"}), 404
 
 
-@app.route("/medical_exams", methods=["GET"])
+@app.route("/api/v1/medical_exams", methods=["GET"])
 def get_medical_exams():
-    medical_exams = MedicalExam.query.all()
+    result = db.session.execute(db.select(MedicalExam))
     medical_exams_list = [
         {"code": medical_exam.code, "name": medical_exam.name}
-        for medical_exam in medical_exams
+        for medical_exam in result
     ]
     return jsonify({"medical_exams": medical_exams_list})
 
 
-@app.route("/medical_exams/<code>", methods=["GET"])
+@app.route("/api/v1/medical_exams/<code>", methods=["GET"])
 def get_medical_exam(code):
-    medical_exam = MedicalExam.query.get(code)
+    result = db.session.execute(db.select(MedicalExam).where(MedicalExam.code == code))
+    medical_exam = result.fetchone()
     if medical_exam:
         medical_exam_data = {"code": medical_exam.code, "name": medical_exam.name}
         return jsonify({"medical_exam": medical_exam_data})
     else:
-        return jsonify({"message": "medical_exam not found"}), 404
+        return jsonify({"message": "Medical exam not found"}), 404
 
 
-@app.route("/appointments", methods=["GET"])
+@app.route("/api/v1/appointments", methods=["GET"])
 def get_appointments():
-    appointments = Appointment.query.all()
+    result = db.session.execute(db.select(Appointment))
     appointments_list = [
         {
             "id_prescription": appointment.id_prescription,
-            "id_ospital": appointment.id_ospital,
+            "id_hospital": appointment.id_ospital,
         }
-        for appointment in appointments
+        for appointment in result
     ]
     return jsonify({"appointments": appointments_list})
 
 
-@app.route("/appointments/<id_prescription>", methods=["GET"])
+@app.route("/api/v1/appointments/<id_prescription>", methods=["GET"])
 def get_appointment(id_prescription):
-    appointment = Appointment.query.get(id_prescription)
+    result = db.session.execute(
+        db.select(Appointment).where(Appointment.id_prescription == id_prescription)
+    )
+    appointment = result.fetchone()
     if appointment:
         appointment_data = {
             "id_prescription": appointment.id_prescription,
-            "id_ospital": appointment.id_ospital,
+            "id_hospital": appointment.id_ospital,
         }
         return jsonify({"appointment": appointment_data})
     else:
-        return jsonify({"message": "appointment not found"}), 404
+        return jsonify({"message": "Appointment not found"}), 404
 
 
-@app.route("/hospitals", methods=["GET"])
+@app.route("/api/v1/hospitals", methods=["GET"])
 def get_hospitals():
-    hospitals = Hospital.query.all()
+    result = db.session.execute(db.select(Hospital))
     hospitals_list = [
         {"id": hospital.id, "address": hospital.address, "name": hospital.name}
-        for hospital in hospitals
+        for hospital in result
     ]
     return jsonify({"hospitals": hospitals_list})
 
 
-@app.route("/hospitals/<id_hospital>", methods=["GET"])
+@app.route("/api/v1/hospitals/<id_hospital>", methods=["GET"])
 def get_hospital(id_hospital):
-    hospital = Hospital.query.get(id_hospital)
+    result = db.session.execute(db.select(Hospital).where(Hospital.id == id_hospital))
+    hospital = result.fetchone()
     if hospital:
         hospital_data = {
             "id": hospital.id,
@@ -220,25 +228,28 @@ def get_hospital(id_hospital):
         }
         return jsonify({"hospital": hospital_data})
     else:
-        return jsonify({"message": "hospital not found"}), 404
+        return jsonify({"message": "Hospital not found"}), 404
 
 
-@app.route("/is_able_to_dos", methods=["GET"])
+@app.route("api/v1/is_able_to_dos", methods=["GET"])
 def get_is_able_to_dos():
-    is_able_to_dos = IsAbleToDo.query.all()
+    result = db.session.execute(db.select(IsAbleToDo))
     is_able_to_dos_list = [
         {
             "id_hospital": is_able_to_do.id_hospital,
             "code_medical_examination": is_able_to_do.code_medical_examination,
         }
-        for is_able_to_do in is_able_to_dos
+        for is_able_to_do in result
     ]
     return jsonify({"is_able_to_dos": is_able_to_dos_list})
 
 
-@app.route("/is_able_to_dos/<id_is_able_to_do>", methods=["GET"])
+@app.route("api/v1/is_able_to_dos/<id_is_able_to_do>", methods=["GET"])
 def get_is_able_to_do(id_is_able_to_do):
-    is_able_to_do = IsAbleToDo.query.get(id_is_able_to_do)
+    result = db.session.execute(
+        db.select(IsAbleToDo).where(IsAbleToDo.id_hospital == id_is_able_to_do)
+    )
+    is_able_to_do = result.fetchone()
     if is_able_to_do:
         is_able_to_do_data = {
             "id_hospital": is_able_to_do.id_hospital,
@@ -246,7 +257,7 @@ def get_is_able_to_do(id_is_able_to_do):
         }
         return jsonify({"is_able_to_do": is_able_to_do_data})
     else:
-        return jsonify({"message": "is_able_to_do not found"}), 404
+        return jsonify({"message": "Is able to do not found"}), 404
 
 
 # Similar routes can be created for other tables (Prescription, Patient, MedicalExam, Appointment, Hospital, IsAbleToDo)
