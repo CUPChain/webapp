7ed2500f9ccfc918afb97b64671a18913097cb8a
antoandgar <antonioandgargiulo@gmail.com>
2024-01-06 00:19:38 +0100

    clean on main and fix on wrong .gitignore

--- backend/main.py
+++ backend/main.py
@@ -13,35 +13,5 @@ def not_found(e):
     return app.send_static_file("index.html")
 
 
-# @app.route("/login/")
-# def login():
-#     # here we have the login page
-
-#     return {"login": "bla bla"}
-
-
-# @app.route("/appointment/")  # add <user_id> to the path
-# def book_appointments():
-#     # here we have the booked appointment page
-#     # and the list of available prescriptions
-
-#     return {"appointment": "bla bla"}
-
-
-# @app.route("/reservation_info/")  # add <reservation_id> to the path
-# def reservation_info():
-#     # here we have the information about the reservation
-#     # ith the qr and the stuff about the verification on the blockchain
-
-#     return {"reservation_info": "bla bla"}
-
-
-# @app.route("/account/")  # add <user_id> to the path
-# def account():
-#     # here we have the account page
-
-#     return {"account": "bla bla"}
-
-
 if __name__ == "__main__":
     app.run(debug=True)
