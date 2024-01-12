44b06cb6df4a916d3d6c05c2bc5818cac6983757
Davide Gabrielli <davide.gabrielli_@libero.it>
2024-01-12 18:33:43 +0100

    Enable CORS for all routes in development

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 from .config import config
 
@@ -10,6 +11,8 @@ migrate = Migrate()
 
 def create_app(config_mode):
     app = Flask(__name__, static_folder="../frontend/build", static_url_path="/")
+    if config_mode == "development":
+        CORS(app) # This will enable CORS for all routes
     app.config.from_object(config[config_mode])
 
     db.init_app(app)
