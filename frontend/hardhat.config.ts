5cd594b929ca224e541cd17d5348a29f682db38e
Riccardo Mancini <riccardo.mancini01@gmail.com>
2024-01-13 18:28:12 +0100

    Add hardhat command to list accounts

--- frontend/hardhat.config.ts
+++ frontend/hardhat.config.ts
@@ -1,4 +1,4 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
@@ -12,5 +12,27 @@ const config: HardhatUserConfig = {
     }
   }
 };
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+  const provider = hre.ethers.provider;
+
+  const config_accounts = hre.network.config.accounts
+  var index = 0
+
+  for (const account of accounts) {
+    //printa solo la key del primo address :( non so come modificare il provider per printare anche le altre
+    const privKey = hre.ethers.Wallet.fromPhrase((config_accounts as any).mnemonic, provider).privateKey;
+
+    console.log(
+      "%s {%s} (%i ETH)",
+      account.address,
+      privKey,
+      hre.ethers.formatEther(await provider.getBalance(account.address))
+    )
+    
+    index++
+  }
+});
 
 export default config;
