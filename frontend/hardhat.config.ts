807063575c283daf6bcbd311f3791c18fd51b902
Elios Buzo <eliosbuzo@hotmail.it>
2024-01-18 19:41:48 +0100

    Minor import fixing

--- frontend/hardhat.config.ts
+++ frontend/hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-ethers";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
