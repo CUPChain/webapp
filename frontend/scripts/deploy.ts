85ce963e8b512a95e39d17c9d32dd435865e2fe6
Riccardo Mancini <riccardo.mancini01@gmail.com>
2024-01-18 07:36:31 +0100

    Remove example contracts

--- frontend/scripts/deploy.ts
+++ frontend/scripts/deploy.ts
@@ -1,32 +1,6 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = ethers.parseEther("0.001");
-
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
-
-  // ---
-  const greeter = await ethers.deployContract("Greeter", ["Hello WOrld"]);
-  await greeter.waitForDeployment();
-
-  console.log(
-    `contract greeter successfully deployed to ${greeter.target}`
-  );
-
-  // ---
   const prescrTokens = await ethers.deployContract("PrescriptionTokens", []);
   await prescrTokens.waitForDeployment();
 
