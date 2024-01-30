5dd8f5343ac665edfe17c7d7ab8423c88f7798b7
antoandgar <antonioandgargiulo@gmail.com>
2024-01-30 10:45:37 +0100

    added UML

--- backend/__init__.py
+++ backend/__init__.py
@@ -12,7 +12,7 @@ migrate = Migrate()
 def create_app(config_mode):
     app = Flask(__name__, static_folder="../frontend/build", static_url_path="/")
     if config_mode == "development":
-        CORS(app) # This will enable CORS for all routes
+        CORS(app)  # This will enable CORS for all routes
     app.config.from_object(config[config_mode])
 
     db.init_app(app)
---
+++ uml/README.md
@@ -0,0 +1,6 @@
+# UML Diagrams
+
+To visualize the diagrams, copy paste the text inside te .plantuml files at the following link:
+
+https://www.plantuml.com/plantuml/uml/
+
