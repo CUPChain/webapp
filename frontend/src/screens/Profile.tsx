ed185cb2acfd5041a00d0916362680d6bd92724c
antoandgar <antonioandgargiulo@gmail.com>
2024-02-04 09:46:54 +0100

    an idea for FE, fix me pls =)

--- frontend/src/screens/Profile.tsx
+++ frontend/src/screens/Profile.tsx
@@ -4,11 +4,10 @@ import 'typeface-titillium-web';
 import 'typeface-roboto-mono';
 import 'typeface-lora';
 import Layout from '../components/Layout';
-import { Section, Row, Card, CardBody, Input } from 'design-react-kit';
+import { Section, Row, Card, CardBody, Input, Col, AvatarIcon } from 'design-react-kit';
 import { AccountType } from '../types';
 import { BACKEND_URL } from '../constants';
 import CardTitleLoad from '../components/CardTitleLoad';
-
 
 const Profile = () => {
     const [account, setAccount] = useState<AccountType>({
@@ -18,7 +17,21 @@ const Profile = () => {
         city: "",
         cap: ""
     });
+
+    const getMyAccountAvatar = (role: string) => {
+        if (role === 'patient') {
+            return 'https://picsum.photos/id/1025/400/400.jpg'
+            // 'https://randomuser.me/api/portraits/men/33.jpg';
+        } else if (role === 'doctor') {
+            return 'https://randomuser.me/api/portraits/lego/3.jpg';
+        } else if (role === 'hospital') {
+            return 'https://picsum.photos/id/764/400/400.jpg';
+        }
+        return '';
+    };
+
     const [loaded, setLoaded] = useState(false);
+    const [role, setRole] = useState<string>(localStorage.getItem('role') || '');
 
     useEffect(() => {
         const fetchData = async () => {
@@ -40,6 +53,7 @@ const Profile = () => {
             const data = await response.json() as AccountType;
             setAccount(data);
             setLoaded(true);
+            setRole(localStorage.getItem('role') || '');
         };
         fetchData();
     }, []);
@@ -49,54 +63,63 @@ const Profile = () => {
             <Section color='muted' className='mt-1'>
                 <Card noWrapper className='card-bg card-big'>
                     <CardBody>
-                        <CardTitleLoad title='Dettagli Account' loaded={loaded} />
-                        <Row style={{ marginTop: '3rem' }}>
-                            <Input
-                                type='text'
-                                label='Nome'
-                                id='formNome'
-                                wrapperClassName='col'
-                                readOnly
-                                value={account.name}
-                            />
-                            {account.surname !== undefined && account.surname !== '' &&
-                                <Input
-                                    type='text'
-                                    label='Cognome'
-                                    id='formCognome'
-                                    wrapperClassName='col'
-                                    readOnly
-                                    value={account.surname}
-                                />
-                            }
-                        </Row>
                         <Row>
-                            <Input
-                                type='text'
-                                label='Indirizzo'
-                                id='formCitta'
-                                wrapperClassName='col'
-                                readOnly
-                                value={account.address}
-                            />
-                        </Row>
-                        <Row>
-                            <Input
-                                type='text'
-                                label='Comune'
-                                id='formComune'
-                                wrapperClassName='col'
-                                readOnly
-                                value={account.city}
-                            />
-                            <Input
-                                type='text'
-                                label='CAP'
-                                id='formCAP'
-                                wrapperClassName='col'
-                                readOnly
-                                value={account.cap}
-                            />
+                            <Col xs='2'>
+                                <AvatarIcon size='xxl'>
+                                    <img src={getMyAccountAvatar(role)} alt='Account Avatar' />
+                                </AvatarIcon>
+                            </Col>
+                            <Col>
+                                <CardTitleLoad title='Dettagli Account' loaded={loaded} />
+                                <Row style={{ marginTop: '3rem' }}>
+                                    <Input
+                                        type='text'
+                                        label='Nome'
+                                        id='formNome'
+                                        wrapperClassName='col'
+                                        readOnly
+                                        value={account.name}
+                                    />
+                                    {account.surname !== undefined && account.surname !== '' &&
+                                        <Input
+                                            type='text'
+                                            label='Cognome'
+                                            id='formCognome'
+                                            wrapperClassName='col'
+                                            readOnly
+                                            value={account.surname}
+                                        />
+                                    }
+                                </Row>
+                                <Row>
+                                    <Input
+                                        type='text'
+                                        label='Indirizzo'
+                                        id='formCitta'
+                                        wrapperClassName='col'
+                                        readOnly
+                                        value={account.address}
+                                    />
+                                </Row>
+                                <Row>
+                                    <Input
+                                        type='text'
+                                        label='Comune'
+                                        id='formComune'
+                                        wrapperClassName='col'
+                                        readOnly
+                                        value={account.city}
+                                    />
+                                    <Input
+                                        type='text'
+                                        label='CAP'
+                                        id='formCAP'
+                                        wrapperClassName='col'
+                                        readOnly
+                                        value={account.cap}
+                                    />
+                                </Row>
+                            </Col>
                         </Row>
                     </CardBody>
                 </Card>
